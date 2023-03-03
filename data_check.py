542127ae8b5e8f8d6289ee1c1e52509c496795d9
tak-jj <takij37@gmail.com>
2023-03-03 15:29:33 +0900

    data check

--- data_check.py
+++ data_check.py
@@ -2,58 +2,45 @@ import json
 import os
 import glob
 
-cat_json_list = glob.glob(os.path.join('D:/Data/Training/Label/Cat', '*', '*', '*.json'))
-dog_json_list = glob.glob(os.path.join('D:/Data/Training/Label/Dog', '*', '*', '*.json'))
+cat_image_list = glob.glob(os.path.join('D:\Data\Training\CAT\image', '*', '*', '*', '*.jpg'))
 
-print('cat : {}, dog : {}'.format(len(cat_json_list), len(dog_json_list)))
+cat_valid = 0
+cat_null = 0
+cat_data = dict()
 
-exit()
-cat_label = {}
-cat_except = 0
-for json_path in cat_json_list:
-    label = json_path.split('\\')[-2]
+print('Collecting start!')
+
+for image_path in cat_image_list:
+    file_name = image_path.split('\\')[-2]
+    pose = image_path.split('\\')[-3]
+    json_path = os.path.join('D:/Data/Training/CAT/label', pose, pose, file_name + '.json')
     with open(json_path, 'r', encoding='utf-8') as f:
-        temp_dict = json.load(f)
-    for frame in temp_dict['annotations']:
-        flag = True
-        for keypoint, coord in frame['keypoints'].items():
-            if coord == None:
-                flag = False
-        if flag:
-            if label not in cat_label.keys():
-                cat_label[label] = 1
+        label_dict = json.load(f)
+    
+    image_name = image_path.split('\\')[-1]
+    image_frame = image_name.split('_')[1]
+    
+    for frame in label_dict['annotations']:
+        if frame['frame_number'] == int(image_frame):
+            flag = True
+            for keypoint, coord in frame['keypoints'].items():
+                if coord == None:
+                    flag = False
+            if flag:
+                cat_valid += 1
+                if pose not in cat_data.keys():
+                    cat_data[pose] = 1
+                else:
+                    cat_data[pose] += 1
             else:
-                cat_label[label] += 1
-        else:
-            cat_except += 1
-    print('{} done'.format(json_path.split('\\')[-1]))
+                cat_null += 1
+            
+            if cat_valid % 1000 == 0:
+                print('{} data collected'.format(cat_valid))
+                print('{} data failed'.format(cat_null))
 
-with open('./cat_label.json', 'w') as f:
-    json.dump(cat_label, f, indent="\t")
+with open('./cat_data.json', 'w') as f:
+    json.dump(cat_data, f, indent='\t')
 
-print('{} data excepted'.format(cat_except))
-
-dog_label = {}
-dog_except = 0
-for json_path in dog_json_list:
-    label = json_path.split('\\')[-2]
-    with open(json_path, 'r', encoding='utf-8') as f:
-        temp_dict = json.load(f)
-    for frame in temp_dict['annotations']:
-        flag = True
-        for keypoint, coord in frame['keypoints'].items():
-            if coord == None:
-                flag = False
-        if flag:
-            if label not in dog_label.keys():
-                dog_label[label] = 1
-            else:
-                dog_label[label] += 1
-        else:
-            dog_except += 1
-    print('{} done'.format(json_path.split('\\')[-1]))
-
-with open('./dog_label.json', 'w') as f:
-    json.dump(dog_label, f, indent="\t")
-
-print('{} data excepted'.format(dog_except))
+with open('./cat_result.txt', 'w') as f:
+    f.write('Cat data : {}, null data : {}'.format(cat_valid, cat_null))
