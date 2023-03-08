94021081796fb1008c0bc65c62290bd22f1a0808
YUYUJIN <wlsyou98@gmail.com>
2023-03-08 10:15:24 +0900

    prepare train

---
+++ rcnn_train/train.py
@@ -0,0 +1,35 @@
+import torch
+from model import get_model
+from torch.utils.data import DataLoader
+
+from animal_keypoint.utils import collate_fn
+from animal_keypoint.engine import train_one_epoch,evaluate
+from customdataset import ClassDataset,train_transform
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+KEYPOINTS_FOLDER_TRAIN = 'D:/test/train'
+KEYPOINTS_FOLDER_TEST = 'D:/test/val'
+
+dataset_train = ClassDataset(KEYPOINTS_FOLDER_TRAIN, transform=train_transform(), demo=False)
+dataset_test = ClassDataset(KEYPOINTS_FOLDER_TEST, transform=None, demo=False)
+
+data_loader_train = DataLoader(dataset_train, batch_size=1, shuffle=True, collate_fn=collate_fn)
+data_loader_test = DataLoader(dataset_test, batch_size=1, shuffle=False, collate_fn=collate_fn)
+
+model = get_model(num_keypoints = 15,num_classes=2)
+model.to(device)
+
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0005)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.3)
+num_epochs = 10
+
+# Run model
+for epoch in range(num_epochs):
+    train_one_epoch(model, optimizer, data_loader_train, device, epoch, print_freq=1000)
+    lr_scheduler.step()
+    evaluate(model, data_loader_test, device)
+    
+# Save model weights after training
+torch.save(model.state_dict(), 'keypointsrcnn_weights.pth')
